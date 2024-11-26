8aecd42b3310d6c2dc3ed403ef2e626cbd81f9c8
spacenot4me <melentev022@gmail.com>
2024-11-26 13:56:11 +0500

    Comm
    Comm

---
+++ models/books.js
@@ -0,0 +1,20 @@
+module.exports = [
+    { isbn: "54321", title: "Adventures in Code", author: "John Doe", reviews: [] },
+    { isbn: "98765", title: "Mystery of the Ocean", author: "Jane Smith", reviews: [] },
+    { isbn: "11223", title: "Secrets of the Jungle", author: "Mark Twain", reviews: [] },
+    { isbn: "33445", title: "Space Odyssey", author: "Arthur C. Clarke", reviews: [] },
+    { isbn: "55667", title: "Beyond the Horizon", author: "Emily Bronte", reviews: [] },
+    { isbn: "77889", title: "Path to Glory", author: "H. P. Lovecraft", reviews: [] },
+    { isbn: "99001", title: "Echoes of Eternity", author: "Isaac Asimov", reviews: [] },
+    { isbn: "22334", title: "The Lost World", author: "Michael Crichton", reviews: [] },
+    { isbn: "44556", title: "Parallel Universes", author: "Stephen Hawking", reviews: [] },
+    {
+        isbn: "66778",
+        title: "Chronicles of Time",
+        author: "Ursula K. Le Guin",
+        reviews: [
+            { username: "Ivy", review: "Beautifully written and thought-provoking." },
+            { username: "Jack", review: "A literary gem!" }
+        ]
+    }
+];
